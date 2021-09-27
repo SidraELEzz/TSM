9ac8b416b3f38956670f84dfc95292eb793422a1
muntazir halim <86202351+muntazir-halim@users.noreply.github.com>
2021-09-27 21:32:05 +0300

    Create checkerUS.py

---
+++ TSM/TSMRS/checkerUS.py
@@ -0,0 +1,19 @@
+class check:
+	def Instagram(self,user):
+		print(user)		
+		pass
+	def Facebook(self):
+		print(user)		
+		pass
+	def Snapchat(self,user):
+		print(user)		
+		pass
+	def Telegram(self,user):
+		print(user)		
+		pass
+	def Twitter(self,user):
+		print(user)		
+		pass
+	def YouTube(self,user):
+		print(user)		
+		pass
